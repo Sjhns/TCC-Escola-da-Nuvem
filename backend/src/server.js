8c3cf17513a74464d82a11ae89394063f53a791f
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-03-03 23:37:58 -0300

    add origin para config do cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const corsOptions = {
   origin: "https://tcc-aws-2.vercel.app",
 }
-app.use(cors(corsOptions))
+app.use(cors)
 
 app.use(express.json())
 app.use(router)
